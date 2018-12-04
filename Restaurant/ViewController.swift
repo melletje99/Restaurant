90946564bfe45955d937201a8819d5743749a081
melletje99 <wesselmel@gmail.com>
2018-12-04 14:59:01 +0100

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Wessel Mel on 04/12/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
